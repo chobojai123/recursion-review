3ebeef096de288377494ef7e86670a4d446a9180
chobojai123 <davidcheng1290@gmail.com>
2018-03-05 15:06:55 -0800

    Added getElementsByClassName and stringifyJSON functions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  var nodes = [];
+  node = node || document.body;
+  
+  if (node.classList && node.classList.contains(className)) {
+    nodes.push(node);
+  } 
+
+  if (node.childNodes.length > 0) {
+    for (var index = 0; index < node.childNodes.length; index++) {
+       nodes = nodes.concat(getElementsByClassName(className, node.childNodes[index]));
+    }
+  }
+  return nodes;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,20 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"'
+  } else if (typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+    return '[' + obj.map(function(item) {
+      return stringifyJSON(item);}).join(',') + ']';
+    } else if (obj === null) {
+      return 'null';
+    } else {
+      return '{' + Object.keys(obj).filter(function(key) {
+        return typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined';}).map(function(key) {
+        return stringifyJSON(key) + ':' + stringifyJSON(obj[key])}).join(',') + '}'
+    }
+  } else {
+    return '' + obj;  
+  }
 };
